b21ce9330d36b2a2d18e2d7595477cdb3f3db6e0
scain-ui <scain@midland.k12.ia.us>
2025-10-17 17:18:34 +0000

    3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var quote = "...ask not what your country can do for you — ask what you can do for your country.";
 
 //Add code below
-var quoteLength = quote_length;
+var quote.Length = quoteLength;
 var twentysecondletter = quote[21];
 var output = '[${quoteLenght}] ${quoteLenght}';
 
