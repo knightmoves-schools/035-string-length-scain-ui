66ae52e8daff521cc13c9aca2805bb6c5a2864c3
scain-ui <scain@midland.k12.ia.us>
2025-10-17 17:16:42 +0000

    2

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ var quote = "...ask not what your country can do for you — ask what you can do
 var quoteLength = quote_length;
 var twentysecondletter = quote[21];
 var output = '[${quoteLenght}] ${quoteLenght}';
+
+document.getElementById("result").innerHTML = output;
