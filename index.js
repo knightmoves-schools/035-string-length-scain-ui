aae09865d868ad904fd39aba294d644c5ff66a23
scain-ui <scain@midland.k12.ia.us>
2025-10-20 16:58:15 +0000

    7

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var quote = "...ask not what your country can do for you — ask what you can do for your country.";
 
 //Add code below
-var quoteLength = quote.Length;
+var quoteLength = quote.length;
 var twentySecondLetter = quote[21];
+// create a string variable named output that contains the quoteLength
 var output = `[${twentySecondLetter}] ${quoteLength}`;
-
-document.getElementById("result").innerHTML = output;
+// modify output to also contain the twentySecondLetter between two brackets ([])
+// assign the innerHTML of the HTML element with the id result to the output
+document.getElementById('result').innerHTML = output;
